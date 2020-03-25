6d3ed054ee5f4f878580f78759c7048142267605
lejdiprifti <priftilejdi@gmail.com>
2020-03-25 16:49:38 +0100

    implemented the basic logic

--- game.py
+++ game.py
@@ -15,7 +15,8 @@ class Game():
         if choice != 'y' and choice != 'n':
             self.begin_game()
         elif choice == 'y':
-            self.start_game()
+            player = self.identify_player()
+            self.start_game(player)
         else:
             pass
         
@@ -30,14 +31,19 @@ class Game():
                     print('Wrong amount of money provided. Please provide a valid amount.')
     
     def place_bet(self, player):
-        try:
-            bet = int(input(f"Hello, {player.name}! What's your bet for this hand? "))
-            return bet
-        except:
-            print('Please, provide a valid bet.')
-            self.place_bet(player)
+        while True:
+            try:
+                bet = int(input(f"Hello, {player.name}! What's your bet for this hand? "))
+                if bet < player.account:
+                    player.place_bet(bet)
+                    return bet
+                else:
+                    print('Bet cannot be larger than your current account.')
+            except:
+                print('Please, provide a valid bet.')
+                self.place_bet(player)
 
-    def handle_first_cards(self,cards):
+    def handle_first_cards(self,cards,player):
         '''
         INPUT: an array of cards
         OUTPUT: adds two cards to the players' cards array and computer's cards array
@@ -45,11 +51,8 @@ class Game():
         for i in range(0,3,2):
             self.playerCards.append(cards.pop(i))
             self.computerCards.append(cards.pop(i))
-
-
-    def hit_or_stay(self, player):
-        choice = input(f'{player.name}, do you want to hit or stay? h/s')
-        return choice
+        print(f'{player.name}, you have this cards.\n{self.playerCards[0].name} {self.playerCards[1].suit}\n{self.playerCards[1].name} {self.playerCards[1].suit}')
+        print(f'Dealer, you have this cards: \n{self.computerCards[0].name} {self.computerCards[0].suit} \n Hidden')
 
 
     def check_sum(self, userArray):
@@ -57,15 +60,15 @@ class Game():
         for card in userArray:
             sum = sum + card.points
         if sum < 21:
-            return sum
+            return 'CONTINUE'
         elif sum == 21:
             return 'WON'
         else:
             return 'BUST'
     
     def hit(self, cards, userArray):
-        self.userArray.append(cards.pop())
-        self.check_sum(self.userArray)
+        userArray.append(cards.pop())
+        return self.check_sum(userArray)
             
     def check_result(self):
         playerSum = 0 
@@ -76,25 +79,63 @@ class Game():
             computerSum = computerSum + card.points
         return playerSum > computerSum
         
+    def validate_hit(self, hitResult, player, bet):
+        if hitResult == 'WON':
+            print(f'Congrats, {player.name}! You won!')
+            player.win_bet(bet)
+            print(f'{player.name}, you currently have {player.account}$.')
+        elif hitResult == 'BUST':
+            if player.name == 'Dealer':
+                 print(f'Congrats! You won {bet}$.')
+            else:
+                print(f'Whoops! {player.name}, you lost this hand.')
+                print(f'{player.name}, you currently have {player.account}$.')
+    
+    def print_card_arrays(self, array):
+        for card in array:
+            print(f'Card: {card.name} {card.suit}')
 
-    def start_game(self):
-        player = self.identify_player()
+    def start_game(self, player):
+        self.playerCards = []
+        self.computerCards = []
+        dealer = Player('Dealer',0)
+        # create the deck of cards
         deck = Deck()
         cards = deck.create_card_array()
+        # place the bet
         bet = self.place_bet(player)
         # distribute the first two cards to the players
-        self.handle_first_cards(cards)
-        #ask for hit or stay
+        self.handle_first_cards(cards,player)
+        # ask for hit or stay
         while True:
-            playerChoice = self.hit_or_stay(self, player)
+            playerChoice = input(f'{player.name}, do you want to hit or stay? h/s ')
             if playerChoice == 'h':
-                self.hit(cards, self.playerCards)
-                self.hit(cards, self.computerCards)
+                playerResult = self.hit(cards, self.playerCards)
+                self.validate_hit(playerResult,player,bet)
+                print(f'{player.name}')
+                self.print_card_arrays(self.playerCards)
+                if playerResult == 'WON' or playerResult == 'BUST':
+                    break
+                computerResult = self.hit(cards, self.computerCards)
+                self.validate_hit(computerResult,dealer,bet)
+                print('Dealer')
+                self.print_card_arrays(self.computerCards)
+                if computerResult == 'WON' or computerResult == 'BUST':
+                    break
             elif playerChoice == 's':
+                # print the cards each player currently has
+                print(f'{player.name}')
+                self.print_card_arrays(self.playerCards)
+                print('Dealer')
+                self.print_card_arrays(self.computerCards)
+                # check the sum of both players and if True, finish this hand
                 if self.check_result():
                     player.win_bet(bet)
                     print(f'Congrats! You won {bet}$.')
-                    break
-        self.start_game()
+                else:
+                    print(f'Whoops! You lost the bet.')
+                print(f'{player.name}, you currently have {player.account}$.')
+                break
+        self.start_game(player)
 
 game = Game()
